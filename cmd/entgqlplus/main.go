a7a7e589a150e1c5c0431186886e59b3fe963215
Oussama Sethoum <os.sethoum@gmail.com>
2022-12-22 09:58:32 +0100

    add cmd

---
+++ cmd/entgqlplus/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"embed"
+	"log"
+	"os"
+	"path"
+	"text/template"
+
+	"entgo.io/ent/entc/gen"
+)
+
+type (
+	templateData struct {
+	}
+
+	file struct {
+		Path   string
+		Buffer string
+	}
+)
+
+//go:embed templates
+var assets embed.FS
+
+func main() {
+	data := &templateData{}
+	files := []file{
+		{
+			Path:   "gqlgen.yaml",
+			Buffer: parseTemplate("gqlgen.go.yml", data),
+		},
+		{
+			Path:   "ent/generate/entc.go",
+			Buffer: parseTemplate("entc.go.yml", data),
+		},
+		{
+			Path:   "ent/generate/generate.go",
+			Buffer: parseTemplate("generate.go.yml", data),
+		},
+	}
+
+	writeFiles(files)
+}
+
+func writeFiles(files []file) {
+	for _, f := range files {
+		err := os.MkdirAll(path.Dir(f.Path), 0666)
+		catch(err)
+		os.WriteFile(f.Path, []byte(f.Buffer), 0666)
+	}
+}
+
+func parseTemplate(filename string, data *templateData) string {
+	buffer, err := assets.ReadFile("templates/" + filename)
+	catch(err)
+	t, err := template.New(filename).Funcs(gen.Funcs).Parse(string(buffer))
+	catch(err)
+	out := bytes.Buffer{}
+	err = t.Execute(&out, data)
+	catch(err)
+	return out.String()
+}
+
+func catch(err error) {
+	if err != nil {
+		log.Fatalln("entgqlplus:", err)
+	}
+}
---
+++ cmd/entgqlplus/templates/generate.go.tmpl
@@ -0,0 +1,8 @@
+package ent
+
+import (
+	_ "entgo.io/contrib/entgql"
+	_ "entgo.io/ent/entc"
+)
+
+//go:generate go run entc.go
